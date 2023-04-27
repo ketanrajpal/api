17d53128b4d23d4fe31452f74f1e5284eab6268d
Ketan Rajpal <ketanrajpal@gmail.com>
2023-04-27 23:33:00 +0100

    added support for body parser

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express'
 import compression from 'compression'
 import helmet from 'helmet'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
 
 const app = express()
 
 app.use(compression())
 app.use(helmet())
 app.disable('x-powered-by')
+app.use(cookieParser())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
