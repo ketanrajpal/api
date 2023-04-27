bc884a96f055ba92cd156b7886858b4ffb0e1eb2
Ketan Rajpal <ketanrajpal@gmail.com>
2023-04-28 00:15:05 +0100

    initialised apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,74 @@
 import express from 'express'
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
 import compression from 'compression'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
+import https from 'https'
+import fs from 'fs'
+import cors from 'cors'
 
-const app = express()
+const application = async () => {
+    const app = express()
 
-app.use(compression())
-app.use(helmet())
-app.disable('x-powered-by')
-app.use(cookieParser())
+    app.use(compression())
+    app.use(helmet())
+    app.disable('x-powered-by')
+    app.use(cookieParser())
+    app.use(cors())
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    const server = https.createServer(
+        {
+            key: fs.readFileSync(`${__dirname}/certs/localhost.key`),
+            cert: fs.readFileSync(`${__dirname}/certs/localhost.crt`),
+        },
+        app
+    )
 
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000')
-})
+    const books = [
+        {
+            title: 'The Awakening',
+            author: 'Kate Chopin',
+        },
+        {
+            title: 'City of Glass',
+            author: 'Paul Auster',
+        },
+    ]
 
-export default app
+    const resolvers = {
+        Query: {
+            books: () => books,
+        },
+    }
+
+    const apolloServer = new ApolloServer({
+        typeDefs: `
+        type Book {
+            title: String
+            author: String
+          }
+
+          type Query { 
+            books: [Book]
+          }
+        `,
+        resolvers,
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer: server })],
+    })
+
+    await apolloServer.start()
+
+    app.use('/graphql', expressMiddleware(apolloServer))
+
+    server.listen({ port: 4000 })
+
+    return server
+}
+
+application()
